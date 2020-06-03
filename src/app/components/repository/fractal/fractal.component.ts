429b2798e1bcfb13f2653b45674018eeef7e71ba
Madalina Moise <Madalina.Moise@endava.com>
2020-06-03 13:25:14 +0300

    hovering effect

--- src/app/components/repository/fractal/fractal.component.ts
+++ src/app/components/repository/fractal/fractal.component.ts
@@ -31,22 +31,43 @@ export class FractalComponent implements AfterViewInit {
     this.noOfFractals = this.fractalCoordinates.length;
 
     console.log(this.fractalCoordinates);
+    const tooltip = d3.select('body')
+      .append('div')
+      .style('position', 'absolute')
+      .style('z-index', '10')
+      .style('visibility', 'hidden')
+      .style('font-size', '20px')
+      .style('color', 'rgba(58, 0, 136, 1)');
 
     const svg = d3.select(`#svg${this.fractalSVGId}`)
       .append('svg')
       .attr('width', figWidth)
       .attr('height', figWidth);
 
-    svg.selectAll('foo')
+    svg.selectAll('rect')
       .data(this.fractalCoordinates)
       .enter()
       .append('rect')
+      .attr('id', d => `svg${this.fractalSVGId}-rect${this.fractalCoordinates.indexOf(d)}`)
       .attr('x', d => d.x)
       .attr('y', d => d.y)
       .attr('width', d => d.w)
       .attr('height', d => d.h)
       .style('fill', d => d.color)
-      .style('opacity', 0.5);
+      .style('opacity', 0.8)
+      .style('cursor', 'pointer')
+      .attr('transform', 'translate(0, 0)')
+      .on('mouseover', d => {
+        tooltip.style('visibility', 'visible').text(`@${d.name}`);
+        d3.select(`#svg${this.fractalSVGId}-rect${this.fractalCoordinates.indexOf(d)}`).style('opacity', 0.5);
+      })
+      .on('mousemove', () => {
+        return tooltip.style('top',
+          (d3.event.pageY - 10) + 'px').style('left', (d3.event.pageX + 10) + 'px');
+      })
+      .on('mouseout', d => {
+        tooltip.style('visibility', 'hidden');
+        d3.select(`#svg${this.fractalSVGId}-rect${this.fractalCoordinates.indexOf(d)}`).style('opacity', 0.8);
+      });
   }
-
 }
